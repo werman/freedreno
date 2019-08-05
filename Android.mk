84ae23d177ca6d81c22f399fb967d4f2d92aabdb
Rob Clark <robdclark@chromium.org>
2019-08-05 16:18:42 -0700

    tests-3d: add test-shader-runner

--- Android.mk
+++ Android.mk
@@ -352,6 +352,14 @@ LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := shader-runner
+LOCAL_SRC_FILES := tests-3d/test-shader-runner.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
+LOCAL_CFLAGS := -DBIONIC -std=c99
+LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := test-shader-size
 LOCAL_SRC_FILES := tests-3d/test-shader-size.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
