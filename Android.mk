45544853b08fb435607f0027d08df8e94cf87327
Rob Clark <robdclark@chromium.org>
2019-09-24 13:46:26 -0700

    add test-clear-fbo

--- Android.mk
+++ Android.mk
@@ -72,6 +72,14 @@ LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv2
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := test-clear-fbo
+LOCAL_SRC_FILES := tests-3d/test-clear-fbo.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
+LOCAL_CFLAGS := -DBIONIC -std=c99
+LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := test-compiler
 LOCAL_SRC_FILES := tests-3d/test-compiler.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
